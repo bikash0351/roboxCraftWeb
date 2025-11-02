4bf05003bf7576bc2cedaf4a774fa25750b0738b
Haitomns G <haitomns4173@gmail.com>
2025-11-02 15:09:36 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,21 @@
-import * as React from "react"
 
+"use client";
+
+import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
+    const [isMounted, setIsMounted] = React.useState(false);
+
+    React.useEffect(() => {
+      setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+      return null;
+    }
+    
     return (
       <input
         type={type}
