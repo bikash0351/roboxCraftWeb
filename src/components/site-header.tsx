cae95986c0a63bf2b84f9059f45cc7a93238cda6
Haitomns G <haitomns4173@gmail.com>
2025-11-02 15:44:44 +0000

    OK So what I want to do is that I want to remove the account icon from t

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -52,47 +52,7 @@ export function SiteHeader() {
         </div>
 
         <div className="flex items-center gap-4">
-          {loading ? (
-             <Skeleton className="h-10 w-10 rounded-full" />
-          ) : user ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-                  <Avatar className="h-10 w-10">
-                    {userImage && <AvatarImage src={userImage} alt={user.displayName || "User"} />}
-                    <AvatarFallback>{userInitial.toUpperCase()}</AvatarFallback>
-                  </Avatar>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                  <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">{user.displayName}</p>
-                    <p className="text-xs leading-none text-muted-foreground">
-                      {user.email}
-                    </p>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem asChild>
-                   <Link href="/account">Account</Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={signOut}>
-                  Log out
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          ) : (
-             <Button asChild variant="ghost" className="relative h-10 w-10 rounded-full">
-              <Link href="/login">
-                  <Avatar className="h-10 w-10">
-                    <AvatarFallback>
-                      <User className="h-5 w-5" />
-                    </AvatarFallback>
-                  </Avatar>
-              </Link>
-            </Button>
-          )}
+         
         </div>
       </div>
     </header>
